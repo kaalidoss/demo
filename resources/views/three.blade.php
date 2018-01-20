e9aaeb0c2530f0556a302d2997e5380dd254b65e
kaalidoss.r <malathikaalidoss0607@gmail.com>
2018-01-20 21:34:55 +0530

    confilict is resolved

--- resources/views/three.blade.php
+++ resources/views/three.blade.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
         echo "hi kaiodss";
         ?>
         <html>hi kalidoss</html>
 
-=======
-        echo "hi kaiodss";?>
-<html>hi added</html>
->>>>>>> testfile-added
 
